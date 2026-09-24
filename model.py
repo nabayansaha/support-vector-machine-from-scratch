6e8deb396ae6401f4343f961dd74e103d1b50608
Nabayan Saha <119719905+nabayansaha@users.noreply.github.com>
2026-09-24 13:26:39 +0530

    Step 5: hinge_loss_example

--- model.py
+++ model.py
@@ -58,8 +58,12 @@ def predict_from_scores(scores):
             
     return labels
 
-# Step 5 - hinge_loss_example (not yet solved)
-# TODO: implement
+# Step 5 - hinge_loss_example
+def hinge_loss_example(score, y):
+    # TODO: return the hinge loss for a single example with raw score `score` and label y in {-1, +1}.
+    L = max(0, 1 - y*score)
+
+    return L
 
 # Step 6 - svm_objective (not yet solved)
 # TODO: implement
