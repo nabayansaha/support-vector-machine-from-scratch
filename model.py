257179c7035440780f25bb495dc03564436b71a2
Nabayan Saha <119719905+nabayansaha@users.noreply.github.com>
2026-09-24 12:44:27 +0530

    Step 2: initialize_parameters

--- model.py
+++ model.py
@@ -22,8 +22,18 @@ def standardize_features(x):
 
     return z
 
-# Step 2 - initialize_parameters (not yet solved)
-# TODO: implement
+# Step 2 - initialize_parameters
+import numpy as np
+
+def initialize_parameters(n_features):
+    """Return a dict with 'w' of shape (n_features,) and scalar 'b'."""
+    # TODO: create starting weights and bias for a linear SVM
+    
+    w = [1/n_features]*n_features
+    w, b = np.array(w),0
+    wb = {'w': w, 'b': b}
+    
+    return wb
 
 # Step 3 - compute_scores (not yet solved)
 # TODO: implement
